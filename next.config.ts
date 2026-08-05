7a743c3432199153eb74ced33e6269aea1ddd8eb
Energyshifter8 <temuulenbold1@gmail.com>
2026-08-05 10:41:42 +0800

    still debugging

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
+	skipTrailingSlashRedirect: true,
 	turbopack: {
 		root: path.resolve(__dirname),
 	},
